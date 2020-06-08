2e3461f6e22aba986beffa396a2dad82d48d7850
Nicolas Corbalan <outreef@hotmail.com>
2020-06-08 23:48:40 +0200

    start adding styles

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {AppContainer} from './styles';
+import './App.css';
+
+const App = () => {
+  return <AppContainer>Columns will go here</AppContainer>;
+};
+
+export default App;
